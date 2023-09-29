ff72c18a8fb6534c710873085afc8ac85c2a57e5
Lokeshganni <lokeshganni17@gmail.com>
2023-09-29 15:30:36 +0000

    ganniroutingeg

---
+++ src/components/About/index.js
@@ -0,0 +1,15 @@
+// Write your JS code here
+import './index.css'
+
+const About = () => (
+  <div className="container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/about-blog-img.png"
+      alt="about"
+      className="icon"
+    />
+    <h1 className="heading">About</h1>
+    <p className="para">I Love to create! I am a Frontend Web Developer</p>
+  </div>
+)
+export default About
